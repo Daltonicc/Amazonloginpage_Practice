e4b94401df149a1f40e4141e819e5b506a8fb32d
Daltonicc <ethanpark604@gmail.com>
2021-08-02 12:11:21 +0900

    미션 완성본 ver.4 (로그인 이벤트 기능 추가)

--- app/src/main/java/com/example/amazonloginpage_practice/MainActivity.kt
+++ app/src/main/java/com/example/amazonloginpage_practice/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.amazonloginpage_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.sign
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val username = "ethanpark604@gmail.com"
+        val password = "12346567890"
+
+        signinBtn.setOnClickListener {
+            val inputcontent = emailEdt.text.toString()
+            val inputcontent2 = passwordEdt.text.toString()
+
+            if (inputcontent == username && inputcontent2 == password ) {
+                Toast.makeText(this, "관리자입니다", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
