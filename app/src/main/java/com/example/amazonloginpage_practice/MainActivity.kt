b7004bd76ed6e948f4a3cd946fb2d2b633673426
Daltonicc <ethanpark604@gmail.com>
2021-08-06 19:54:15 +0900

    미션 완성본 ver.4.2 (카멜 표기법 적용)

--- app/src/main/java/com/example/amazonloginpage_practice/MainActivity.kt
+++ app/src/main/java/com/example/amazonloginpage_practice/MainActivity.kt
@@ -2,27 +2,25 @@ package com.example.amazonloginpage_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.math.sign
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val username = "ethanpark604@gmail.com"
-        val password = "12346567890"
-        val name = "Ethan"
+        val userName = "ethanpark604@gmail.com"
+        val userPassword = "12346567890"
+        val adminUser = "Ethan"
 
         signinBtn.setOnClickListener {
             val inputcontent = emailEdt.text.toString()
             val inputcontent2 = passwordEdt.text.toString()
 
 
-            if (inputcontent == username && inputcontent2 == password ) {
-                Toast.makeText(this, "${name} 관리자입니다", Toast.LENGTH_SHORT).show()
+            if (inputcontent == userName && inputcontent2 == userPassword ) {
+                Toast.makeText(this, "${adminUser} 관리자입니다", Toast.LENGTH_SHORT).show()
             }
             else {
                 Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
